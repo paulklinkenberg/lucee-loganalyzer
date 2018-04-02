7514bbd84faad81d8a1f594e79572f972007059e
Zac Spitzer <zac.spitzer@gmail.com>
2018-04-02 17:57:05 +1000

    refactor javascript

--- source/cfml/plugins/js/viewlog.js
+++ source/cfml/plugins/js/viewlog.js
@@ -1,19 +1,19 @@
-$(function(){
-	'use strict';
-	var nl = String.fromCharCode(10);
-	var updateSeverityFilter =  function(){
+'use strict';
+
+var viewLog = {
+	nl: String.fromCharCode(10),
+	updateSeverityFilter: function(){
 		var css = "";
 		var hidden = [];
 		$(".log-severity-filter input:not(:checked)").each(function(){
 			var item = $(this).parent().attr("class");
-			css += "." + item + ".log { display: none; } " + nl;
+			css += "." + item + ".log { display: none; } " + viewLog.nl;
 			hidden.push(item.split("-").pop());
 		});
 		$(".log-severity-filter-css").text(css);
-		history.pushState({},"", updateUrl(null, "severity", hidden.join()) );
-	};
-
-	var updateFileFilter =  function(){
+		history.pushState({},"", viewLog.updateUrl(null, "severity", hidden.join()) );
+	},
+	updateFileFilter: function(){
 		var css = "";
 		var files = {};
 		var selected = {};
@@ -33,33 +33,38 @@ $(function(){
 		} else {
 			for (var f in files){
 				if (files.hasOwnProperty(f) && !files[f])
-					css += ".log-file-filter-" + f.replace(".", "_") + ".log { display: none; } " + nl;
+					css += ".log-file-filter-" + f.replace(".", "_") + ".log { display: none; } " + viewLog.nl;
 			}
 			urlParam = Object.keys(selected).join();
 		}
 		// check the requested files are present in the initial page
 		var loadedFiles = $(".logs").data("files");
-		var newUrl = updateUrl(null, "file", urlParam);
+		var newUrl = viewLog.updateUrl(null, "file", urlParam);
 
-		var reload = reloadRequiredFiles(urlParam, loadedFiles, selected);
+		var reload = viewLog.reloadRequiredFiles(urlParam, loadedFiles, selected);
 		console.log(reload, urlParam, loadedFiles, selected);
 		if (!reload){
 			console.log(urlParam, css);
+			$(".logs-loading").show();
 			$(".log-file-filter-css").text(css);
 			if ($(".logs .log:visible").length === 0){
 				reload = true; // nothing visible, reload anyway
 				//console.log("nothing visible, force reload");
+			} else {
+				$(".logs-loading").hide();
 			}
 		}
 		if (reload){
 			//console.log("reloading");
-			document.location = newUrl;// updateUrl(newUrl, "since", ""); // start over
+			$(".logs-loading").show();
+			setTimeout(function(){
+				document.location = newUrl;// updateUrl(newUrl, "since", ""); // start over
+			}, 100);
 		} else {
-			history.pushState({},"", updateUrl(newUrl, "file", urlParam) );
+			history.pushState({},"", viewLog.updateUrl(newUrl, "file", urlParam) );
 		}
-	};
-
-	var reloadRequiredFiles = function(urlParam, loadedFiles, selected){
+	},
+	reloadRequiredFiles: function(urlParam, loadedFiles, selected){
 		if (!loadedFiles || loadedFiles.length === 0)
 			return false; // page was loaded without a file filter
 		if (urlParam.length === 0 && loadedFiles.length > 0)
@@ -73,9 +78,8 @@ $(function(){
 				return true; // at least one file wasn't loaded
 		}
 		return false; // all requested files are loaded
-	};
-
-	var updateUrl = function(url, param, val){
+	},
+	updateUrl: function(url, param, val){
 		if (!url)
 			url = document.location.href;
 		var key = "&" + param + "=";
@@ -91,86 +95,84 @@ $(function(){
 			url += after;
 		}
 		return url;
-	}
-
-	$(".log-severity-filter INPUT").on("change", updateSeverityFilter);
-	$(".log-file-filter INPUT").on("change", updateFileFilter);
-
-	$(".log-actions INPUT").click(function(){
+	},
+	logActions: function(){
 		var data = $(this).data();
 		switch (data.action){
 			case "poll":
-				pollServerForUpdates();
+				viewLog.pollServerForUpdates();
 				break;
 			case "reload":
 				document.location.reload();
 				break;
 			case "search":
-				doSearch();
+				viewLog.doSearch();
 				break;
 			case "auto-refresh":
-				toggleAutoRefresh($(this));
+				viewLog.toggleAutoRefresh($(this));
 				break;
 			case "expand-all":
-				expandAll();
+				viewLog.expandAll();
 				break;
 			default:
 				//console.warn("unsupported action: " + data.action);
 		};
-	});
-
-	$(".log").click(function(){
+	},
+	toggleExpandLogEntry: function(){
 		var data = $(this).data();
 		var expanded = $(".long-log-" + data.log).is(":VISIBLE");
 		$(".long-log-" + data.log).toggle(!expanded);
-		if (this.nodeName == "A"){
+		if (viewLog.nodeName == "A"){
 			$(this).toggle(!expanded);
 		} else {
 			$(this).find("a.log-expand").toggle(expanded);
 		}
-	});
-	var pollServerForUpdates = function(cb){
+	},
+	pollServerForUpdates: function(cb){
 		var $logs = $(".logs");
 		var fetched = $logs.data("fetched");
-		var url = updateUrl(null, "since", fetched);
+		var url = viewLog.updateUrl(null, "since", fetched);
+		var $loading = $(".logs-loading").show();
 		$.ajax({
-			url: url,
+			url: url + "&xhr=true",
 			type: "GET"
 		}).done(function(data) {
+			$loading.hide();
 			var $data = $(data);
 			var $newLogs = $data.find(".logs");
 			var fetched = $newLogs.data("fetched");
+
 			$logs.data("fetched", $newLogs.data("fetched") );
 			var $new = $newLogs.find(".log");
 			var $status = $("<div>").addClass("logs-update");
 
 			if ($newLogs.length > 0){
 				$logs.prepend($status.text("polled logs " + new Date()),$new);
-				trimLogs();
+				viewLog.trimLogs();
 			} else {
 				$logs.prepend($status.text("polled logs, no updates, " + new Date()));
 			}
 			if (cb)
 				cb();
 		}).error(function(jqXHR){
+			$loading.hide();
 			$(".logs-error").show().html(jqXHR.responseText);
 		});
 		//document.location.reload();
-	};
-	var limit = 2000;
-	var trimLogs = function(){
+	},
+	maxDisplaylimit: 2000,
+	trimLogs: function(){
 		var $logs = $(".logs .log");
 		var total = $logs.length;
-		if ($logs.length > 2000){
-			$logs.slice(2000).remove();
+		if ($logs.length > viewLog.maxDisplaylimit){
+			$logs.slice(viewLog.maxDisplaylimit).remove();
 		}
 		$logs = $(".logs .log");
 		var removed = (total-$logs.length);
 		if (removed)
 			console.log("removed " + removed + " logs for performance");
-	};
-
-	var expandAll = function(){
+	},
+	expandAll: function(){
 		var state = $(this).data("expanded");
 		$(".collapsed-log").toggle(!state);
 		$("a.log-expand").toggle(!state);
@@ -180,9 +182,8 @@ $(function(){
 			$(this).val("Expand All");
 		}
 		$(this).data("expanded", !state);
-	};
-
-	var doSearch = function(){
+	},
+	doSearch: function(){
 		var $el = $(".search-logs");
 		var str = $.trim($el.val()).toLowerCase();
 		if (str.length === 0){
@@ -194,33 +195,40 @@ $(function(){
 				$(this).toggleClass("search-hidden", match);
 			});
 		}
-	};
-	var debounceTimer = null;
+	},
+	debounceTimer: null,
+	refreshTimer: null,
+	toggleAutoRefresh: function($el){
+		if (viewLog.refreshTimer){
+			clearTimeout(viewLog.refreshTimer);
+			$el.val("Auto-refresh");
+			viewLog.refreshTimer = null;
+		} else {
+			$el.val("Stop Auto-refresh");
+			viewLog.refreshTimer = setTimeout(viewLog.autoRefresh(), viewLog.getRefreshPeriod());
+		}
+	},
+	getRefreshPeriod: function(){
+		return Number($(".poll-period").val()) * 1000; // timeouts in ms, drop down has seconds
+	},
+	autoRefresh: function(){
+		refreshTimer = setTimeout(function doPoll (){
+			viewLog.pollServerForUpdates(function callback(){
+				refreshTimer = setTimeout(viewLog.autoRefresh, viewLog.getRefreshPeriod());
+			});
+		}, 5000);
+	}
+};
+
+$(function(){
+	$(".log-severity-filter INPUT").on("change", viewLog.updateSeverityFilter);
+	$(".log-file-filter INPUT").on("change", viewLog.updateFileFilter);
+	$(".log-actions INPUT").on("click", viewLog.logActions);
+	$(".log").on("click", viewLog.toggleExpandLogEntry);
 	$(".search-logs").on("keyup", function(){
-		clearTimeout(debounceTimer);
-		debounceTimer = setTimeout(doSearch, 250);
+		clearTimeout(viewLog.debounceTimer);
+		viewLog.debounceTimer = setTimeout(viewLog.doSearch, 250);
 	}).on("submit", function(){
 		return false;
 	});
-	var refreshTimer = null;
-	var toggleAutoRefresh = function($el){
-		if (refreshTimer){
-			clearTimeout(refreshTimer);
-			$el.val("Auto-refresh");
-			refreshTimer = null;
-		} else {
-			$el.val("Stop Auto-refresh");
-			refreshTimer = setTimeout(autoRefresh(), getRefreshPeriod());
-		}
-	};
-	var getRefreshPeriod = function(){
-		return Number($(".poll-period").val()) * 1000; // timeouts in ms, drop down has seconds
-	};
-	var autoRefresh = function(){
-		refreshTimer = setTimeout(function doPoll (){
-			pollServerForUpdates(function callback(){
-				refreshTimer = setTimeout(autoRefresh, getRefreshPeriod());
-			});
-		}, 5000);
-	};
 });
